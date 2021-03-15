1357002e7742142227d131bc534cd56c36e3ecfd
Prashant Singh <psonlinux@gmail.com>
2021-03-15 22:38:44 +0530

    Simple Chat Client Server App with multiple client

--- src/SimpleChatClient.java
+++ src/SimpleChatClient.java
@@ -1,10 +1,95 @@
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+/*
+ * Run VerySimpleChatServer before before running this client .*/
 public class SimpleChatClient {
+    JTextArea incoming;
+    JTextField outgoing;
+    BufferedReader reader;
+    PrintWriter writer;
+    Socket sock;
+    
     public static void main(String[] args) {
-        
+        SimpleChatClient client = new SimpleChatClient();
+        client.go();
     }
     
     public void go() {
+        JFrame frame = new JFrame("Simple Chat Client");
+        JPanel mainPanel = new JPanel();
+        incoming = new JTextArea(15, 50);
+        incoming.setLineWrap(true);
+        JScrollPane qScroller = new JScrollPane(incoming);
         
+        outgoing = new JTextField(20);
+        
+        JButton sendButton = new JButton("Send");
+        sendButton.addActionListener(new SendButtonListener());
+        
+        mainPanel.add(qScroller);
+        mainPanel.add(outgoing);
+        mainPanel.add(sendButton);
+        
+        setUpNetworking();
+        
+        Thread readerThread = new Thread(new IncomingReader());
+        readerThread.start();
+        
+        frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
+        frame.setSize(800, 500);
+        frame.setVisible(true);
+    }
+    
+    private void setUpNetworking() {
+        try {
+            sock = new Socket("127.0.0.1", 5000);
+            InputStreamReader streamReader = new InputStreamReader(sock.getInputStream());
+            reader = new BufferedReader(streamReader);
+            writer = new PrintWriter(sock.getOutputStream());
+            System.out.println("networking established");
+        }catch(IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    public class SendButtonListener implements ActionListener{
+        public void actionPerformed(ActionEvent ev) {
+            try {
+                writer.println(outgoing.getText());
+                writer.flush();
+            }catch(Exception ex) {
+                ex.printStackTrace();
+            }
+            
+            outgoing.setText("");
+            outgoing.requestFocus();
+        }
+    }
+    
+    public class IncomingReader implements Runnable{
+        public void run() {
+           String message;
+           try {
+               while((message = reader.readLine()) != null) {
+                   System.out.println("read " + message);
+                   incoming.append(message + "\n");
+               }
+           }catch(Exception ex) {
+               ex.printStackTrace();
+           }
+        }
     }
 }
---
+++ src/VerySimpleChatServer.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class VerySimpleChatServer {
+    ArrayList<PrintWriter> clientOutputStreams;
+    
+    public class ClientHandler implements Runnable{
+        BufferedReader reader;
+        Socket sock;
+        public ClientHandler(Socket client) {
+            try {
+                sock = client;
+                InputStreamReader isReader = new InputStreamReader(sock.getInputStream());
+                reader = new BufferedReader(isReader);
+            }catch(Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+        
+        public void run() {
+            String message;
+            try {
+                while((message = reader.readLine()) != null) {
+                    System.out.println("read " + message);
+                    tellEveryone(message);
+                }
+            }catch(Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        VerySimpleChatServer server = new VerySimpleChatServer();
+        server.go();
+    }
+    
+    public void go() {
+        clientOutputStreams = new ArrayList<>();
+        
+        try {
+            ServerSocket serverSock = new ServerSocket(5000);
+            while(true) {
+                Socket clientSocket = serverSock.accept();
+                PrintWriter writer = new PrintWriter(clientSocket.getOutputStream());
+                clientOutputStreams.add(writer);
+                
+                Thread t = new Thread(new ClientHandler(clientSocket));
+                t.start();
+                
+                System.out.println("got a connection ");
+                
+            }
+        }catch(Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    public void tellEveryone(String message) {
+        int n = clientOutputStreams.size();
+        for(int i = 0; i < n; i++) {
+            try {
+                PrintWriter writer = (PrintWriter)clientOutputStreams.get(i);
+                writer.println(message);
+                writer.flush();
+            }catch(Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+}
